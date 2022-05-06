f11c412bcea33fff50d56921cb820b9fcbca5231
v1t0r1an0 <102614473+v1t0r1an0@users.noreply.github.com>
2022-05-06 14:17:30 -0300

    Add files via upload

---
+++ Ex02/Ex02/src/main/java/br/com/senai/guiVitoriano/application/CadastroBean.java
@@ -0,0 +1,10 @@
+package br.com.senai.guiVitoriano.application;
+
+public class CadastroBean {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Ex02/Ex02/src/main/java/br/com/senai/guiVitoriano/application/model/Tipo.java
@@ -0,0 +1,9 @@
+package br.com.senai.guiVitoriano.application.model;
+
+import javax.inject.Named;
+
+@Named("Tipo")
+
+public class Tipo {
+	
+}
